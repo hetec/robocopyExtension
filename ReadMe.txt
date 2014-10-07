30539168eaf2ae3634d62141bb0d00f9ff2206e1
hebner <hebner.tec@gmail.com>
2014-10-07 14:46:01 +0200

    Initial commit
    Initial version of robocopy_extension.ps1 and readme.txt added to repository

---
+++ ReadMe.txt
@@ -0,0 +1,18 @@
+ReadMe
+------
+
+(in process)
+
+Purpose: The application extends the functuanallity [D[Dionality of Robocopy.exe to obtain the possibility for copying a selected range of files/directories in a root directory, what is impossible with Robocopy.exe.
+
+For instance:
+
+Root directory --> root
+in root ---------> sub1, sub2 with some files
+
+Robocopy enables only a selection of particular files (not directories) or
+of the whole content of root
+
+This app enables you to specify particular 
+subdirectories like sub1 or sub2 for copying
+
---
+++ robocopy_extension.ps1
@@ -0,0 +1,61 @@
+﻿param(
+    [switch]$help,
+    [int]$first,
+    [String]$pattern,
+    [switch]$pattern_exclude,
+    [String]$dest = ".\$source(copy)",
+    [String]$source = ".\",
+    [String]$Robo1 = "/MIR",
+    [String]$Robo2 = "/copyall",
+    [String]$Robo3,
+    [String]$Robo4
+)
+
+if($help){
+    Write-Host "`nSyntax: COPY dest source [first|pattern][pattern_exclude][Robo1-4]`n"
+    Write-Host "Standard values:`n`t`tdest=.\source(copy)`n`t`tsource=.\`n`t`tfirst = ALL`n`t`tpattern = ALL`n`t`tpattern_exclude = false`n`t`tRobo1=\MIR`n`t`tRobo2=\copyal`n`t`tlRobo3-4=null"
+
+    Write-Host "`nPossible patterns(not case sensitive):"
+    Write-Host "Begins with --> ^x+"
+    Write-Host "Ends with --> xxx$"
+    Write-Host "Without _ --> _+ and set pattern_exclude = true`n"
+    
+}else{
+    if(!(Test-Path $source)){
+        Write-Host "Wrong source! Check path, please."
+        exit
+    }
+
+    if(Test-Path $dest){
+    Write-Host "Existing directory --> Remove old directory and create new one."
+    Remove-Item $dest -Recurse -Force
+    }else{
+        Write-Host "No existing directory --> Will be created!"
+
+    }
+   
+
+    if($first -and !$pattern){
+        Write-Host "Select $first first items." 
+        $items = Get-ChildItem $source | select -First $first
+    }
+
+    if(!$first -and $pattern){
+        if($pattern_exclude){
+            Write-Host "Use items not matching $pattern."
+            $items = Get-ChildItem $source | Where-Object {!($_.Name -match "$pattern")}
+        }else{
+            Write-Host "Use items matching $pattern."
+            $items = Get-ChildItem $source | Where-Object {$_.Name -match "$pattern"}
+        }
+   
+    }
+    foreach($i in $items){
+        $nameCurrentDir = $i.Name
+        $newSource = "$source\$nameCurrentDir"
+        $newDest = "$dest\$nameCurrentDir"
+        mkdir -Path $newDest
+        Robocopy.exe $newSource $newDest $Robo1 $Robo2 $Robo3 $Robo4
+    }
+}
+
